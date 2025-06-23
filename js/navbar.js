1c6428a8870410177a767ee63e52e5544896d62e
benjaminwp18 <90342856+benjaminwp18@users.noreply.github.com>
2025-06-23 18:58:12 -0400

    WIP styling fixes

--- js/navbar.js
+++ js/navbar.js
@@ -1,13 +1,12 @@
-window.onscroll = function() {f_scroll()};
-
-function f_scroll() {
+window.addEventListener('scroll', () => {
 	if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("nav_logo").style.maxWidth = "80px";
-		document.getElementById("nav_name").style.fontSize = "26px";
-		document.getElementById("nav_desc").style.fontSize = "0px";
-    } else {
-        document.getElementById("nav_logo").style.maxWidth = "120px";
-		document.getElementById("nav_name").style.fontSize = "30px";
-		document.getElementById("nav_desc").style.fontSize = "16px";
-    }
-}
+		document.getElementById("nav-logo").style.maxWidth = "80px";
+		document.getElementById("nav-title-name").style.fontSize = "26px";
+		document.getElementById("nav-title-desc").style.fontSize = "0px";
+	}
+	else {
+		document.getElementById("nav-logo").style.maxWidth = "120px";
+		document.getElementById("nav-title-name").style.fontSize = "30px";
+		document.getElementById("nav-title-desc").style.fontSize = "16px";
+	}
+});
